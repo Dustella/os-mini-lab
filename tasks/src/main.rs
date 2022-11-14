c455b265ec21d813b5c55b938ca486566c212b2c
Dustella <fdnoaivj@outlook.com>
2022-11-15 02:50:16 +0800

    ⚡️ 我已经取得了完全胜利

--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -6,21 +6,22 @@ async fn main() {
     let all_task_list = vec![
         Task {
             name: "".to_string(),
-            duration: 1,
-            start_time: 5,
+            duration: 5,
+            start_time: 0,
         },
         Task {
             name: "".to_string(),
-            duration: 1,
+            duration: 5,
             start_time: 5,
         },
     ];
+    let mut tasklist = TaskList::new();
+    tasklist.start_tasks(&all_task_list).await;
 
-    let (task_list, ls) = TaskList::new(&all_task_list);
-    for i in ls {
-        i.join().unwrap()
-    }
-    println!("end");
+    // for i in ls {
+    //     i.join().unwrap()
+    // }
+    // println!("end");
 
     // run the first task
 }
--- tasks/src/task.rs
+++ tasks/src/task.rs
@@ -1,10 +1,8 @@
-use std::time::Duration;
+use futures::{future::join_all, lock};
+use std::{cmp::Ordering, sync::Arc, time::Duration};
 use tokio::{
-    sync::{
-        mpsc::{self, Receiver, Sender},
-        Mutex,
-    },
-    time::sleep,
+    sync::Mutex,
+    time::{self, sleep},
 };
 
 #[derive(Clone)]
@@ -15,56 +13,71 @@ pub struct Task {
 }
 
 impl Task {
-    fn run(self) {
-        sleep(Duration::from_secs(self.duration));
+    pub async fn run(self) {
+        println!("Running {}", self.clone().name);
+        sleep(Duration::from_secs(self.duration)).await;
     }
-    fn get_response_ratio(&self, time_passed: u64) -> f64 {
+    pub fn get_response_ratio(&self, time_passed: u64) -> f64 {
         let pending_time = time_passed - self.start_time;
         ((pending_time + self.duration) / self.duration) as f64
-    }
-    fn Copy(self) -> Self {
-        self
     }
 }
 
 pub struct TaskList {
-    list: Mutex<Vec<Task>>,
+    list: Arc<Mutex<Vec<Task>>>,
+    timer: time::Instant,
 }
 
 impl TaskList {
-    pub fn new(ls: &Vec<Task>) -> Self {
-        let list = Mutex::new(*ls);
-        for e in ls.clone() {
-            let sp = tokio::spawn(async move {
-                sleep(Duration::from_secs(e.clone().start_time));
-                // push task
-            });
-            add_queue.push(sp);
-        }
-        let mut add_queue = Vec::new();
-        Self { list }
+    pub fn new() -> Self {
+        let list = Arc::new(Mutex::new(Vec::new()));
+        let timer = time::Instant::now();
+        Self { list, timer }
     }
-    pub fn run(self) {}
-    pub fn get_max_ratio_task(&mut self, time_passed: u64) -> Task {
-        let rec = self.getter.try_recv();
-        let mut tasks: Vec<Task> = rec.into_iter().collect();
-        tasks.sort_by(|a, b| {
-            a.get_response_ratio(time_passed)
-                .total_cmp(&b.get_response_ratio(time_passed))
-        });
-        let (max, left) = tasks.split_at(1);
-        for i in left {
-            self.adder.send(i.clone()).unwrap();
+    pub async fn start_tasks(mut self, tasks: &Vec<Task>) {
+        let mut joined_jobs = Vec::new();
+        for e in tasks.clone() {
+            let list_lock = self.list.clone();
+            let sp = tokio::spawn(async move {
+                sleep(Duration::from_secs(e.clone().start_time)).await;
+                let mut task_list = list_lock.lock().await;
+                // push task
+                println!(
+                    "{}s passed, push in task {}.",
+                    e.clone().start_time,
+                    e.clone().name
+                );
+                task_list.push(e);
+            });
+            joined_jobs.push(sp);
         }
-        // let max = &rec
-        //     .max_by(|a, b| {
-        //         a.get_response_ratio(time_passed)
-        //             .total_cmp(&b.get_response_ratio(time_passed))
-        //     })
-        //     .unwrap();
-        // for i in self.getter.iter().filter(|x| x.name != max.clone().name) {
-        //     self.adder.send(i).unwrap();
-        // }
-        max.first().unwrap().clone()
+        let main_task = tokio::spawn(async move {
+            println!("lala");
+            sleep(Duration::from_secs(1)).await;
+            while let Some(task) = self.get_max_ratio_task().await {
+                task.run().await;
+            }
+        });
+        joined_jobs.push(main_task);
+        join_all(joined_jobs).await;
+        println!("All task finished")
+    }
+    pub async fn get_max_ratio_task(&mut self) -> Option<Task> {
+        let time_passed = self.timer.elapsed().as_secs();
+        let mut locked_list = self.list.lock().await;
+        // let mut tasks: Vec<Task> = rec.into_iter().collect();
+        locked_list.sort_by(|a, b| {
+            if (a.get_response_ratio(time_passed) - b.get_response_ratio(time_passed))
+                .is_sign_negative()
+            {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        });
+        let res = locked_list.pop();
+
+        drop(locked_list);
+        res
     }
 }
